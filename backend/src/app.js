ee4f16c7ed0210ffc3e257ed2360861dcc4cb04e
Zelalem Tefera <zedoyetefe@gmail.com>
2025-08-22 15:03:24 +0300

    chore(backend):create express app & feat(backend): setup server and mongoDB connection

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+// backend/src/app.js
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/auth', authRoutes);
+
+export default app;
---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+// backend/src/server.js
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import app from './app.js';
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
