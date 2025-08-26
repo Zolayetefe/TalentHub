dbc9366d422cc5e1dbe5f8afd40959fb6dd436f5
Zelalem Tefera <zedoyetefe@gmail.com>
2025-08-26 23:09:30 +0300

    add frontend url to cors middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cookieParser());
 app.use(cors({
  
-    origin: ['http://localhost:5174','http://localhost:5173'],
+    origin: ['http://localhost:5174','http://localhost:5173',"https://talent-hub-weld.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'PATCH','DELETE', 'OPTIONS'],
     credentials: true
   }));
